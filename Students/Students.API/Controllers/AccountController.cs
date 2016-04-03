e21b8cbb40406fa27657a259b883ac09060e8e8b
Just Script <justscript@outlook.com>
2016-04-03 20:33:18 +0200

    security

--- Students/Students.API/Controllers/AccountController.cs
+++ Students/Students.API/Controllers/AccountController.cs
@@ -3,14 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SessionSecurity.Models;
+using SessionSecurity.Security;
+using SessionSecurity.ViewModels;
 
-namespace Students.API.Controllers
+namespace SessionSecurity.Controllers
 {
     public class AccountController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(AccountViewModel avm)
+        {
+            AccountModel am = new AccountModel();
+            if (string.IsNullOrEmpty(avm.Account.Username)
+             || string.IsNullOrEmpty(avm.Account.Password)
+             || am.Login(avm.Account.Username, avm.Account.Password) == null)
+            {
+                ViewBag.Error = "Account's Invalid";
+                return View("Index");
+            }
+            SessionPersister.Username = avm.Account.Username;
+            return View("Success");
+        }
+
+        public ActionResult Logout()
+        {
+            SessionPersister.Username = string.Empty;
+            return RedirectToAction("Login");
         }
     }
 }
