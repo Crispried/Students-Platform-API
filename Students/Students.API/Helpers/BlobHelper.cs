bb488fbb4a367e17aeb8014595b2fc773f7f0237
justscript <justscript@outlook.com>
2016-04-17 16:43:24 +0200

    blob helper

---
+++ Students/Students.API/Helpers/BlobHelper.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace Students.API.Helpers
+{
+    public static class BlobHelper
+    {
+        public static bool Upload(Image src, string containerName, string fileName)
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                ConfigurationManager.ConnectionStrings["StorageConnectionString"].ConnectionString);
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            CloudBlobContainer container = blobClient.GetContainerReference(containerName);
+
+            if (!container.Exists())
+            {
+                container.Create();
+                container.SetPermissions(new BlobContainerPermissions
+                {
+                    PublicAccess = BlobContainerPublicAccessType.Blob
+                });
+            }
+            
+            CloudBlockBlob blob = container.GetBlockBlobReference(fileName);
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                src.Save(memoryStream, ImageFormat.Png);
+                memoryStream.Position = 0;
+                blob.UploadFromStream(memoryStream);
+                memoryStream.Close();
+            }
+
+            return true;
+        }
+
+        public static bool Move(string containerName, string oldName, string newName)
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                ConfigurationManager.ConnectionStrings["StorageConnectionString"].ConnectionString);
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            CloudBlobContainer container = blobClient.GetContainerReference(containerName);
+
+            if (!container.Exists())
+            {
+                container.Create();
+                container.SetPermissions(new BlobContainerPermissions
+                {
+                    PublicAccess = BlobContainerPublicAccessType.Blob
+                });
+            }
+
+            CloudBlockBlob oldBlob = container.GetBlockBlobReference(oldName);
+            CloudBlockBlob newBlob = container.GetBlockBlobReference(newName);
+
+            newBlob.StartCopy(oldBlob);
+            oldBlob.Delete();
+
+            return true;
+        }
+
+        public static bool Remove(string containerName, string fileName)
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                ConfigurationManager.ConnectionStrings["StorageConnectionString"].ConnectionString);
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            CloudBlobContainer container = blobClient.GetContainerReference(containerName);
+
+            if (!container.Exists())
+            {
+                return false;
+            }
+
+            CloudBlockBlob blob = container.GetBlockBlobReference(fileName);
+            blob.Delete();
+
+            return true;
+        }
+    }
+}
